8833d3ac25e273e8f27cc1f3d763e73ac4e3dc62
Jal Shrestha <79690387+jalshrestha@users.noreply.github.com>
2023-01-19 22:29:49 -0500

    Add files via upload
    This is the coding of Java.

---
+++ Lab1.java
@@ -0,0 +1,27 @@
+/*
+ Author: Jaljala Shrestha Lama
+ Course: CSC260L-003
+ Date: 1/19/2023
+ Assignment: #1
+ Instructor: Brian Michael Sauer
+*/
+public class Lab1 {
+    public static void main(String[] args){
+        String name = "Jal";
+        int age = 18;
+        String message ="Information is not knowledge, knowledge is not wisdom!";
+        System.out.println("Hello "+ name);
+        System.out.println("You are "+age+" years old.\n\n");
+        System.out.println("The message of the day is "+message+ "\n");
+        System.out.println("Done");
+
+    }
+}
+/* Hello Jal
+        You are 18 years old.
+
+
+        The message of the day is Information is not knowledge, knowledge is not wisdom!
+
+        Done
+*/
